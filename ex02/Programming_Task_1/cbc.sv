2d513323dccc8990e41b004c5187f9ab9e489346
AlexanderRitter02 <42546705+AlexanderRitter02@users.noreply.github.com>
2025-05-18 15:19:50 +0200

    Ex02 2.4: Implement decryption

--- ex02/Programming_Task_1/cbc.sv
+++ ex02/Programming_Task_1/cbc.sv
@@ -2,6 +2,7 @@ module cbc # (
   parameter n = 1,
   parameter m = 3
 ) (
+  input logic enc_dec,
   input logic  [n*m*8-1:0] message,
   input logic  [n*8-1:0] key,
   output logic [n*m*8-1:0] out
@@ -22,11 +23,18 @@ end
 // Generate remaining chunks
 for (genvar chunk = 1; chunk < m; chunk++) begin: gen_chunk_xors
 
+  logic [n*8-1:0] current_key;
+
+  // Decide whether to select output (encryption, enc_dec=0) or chunk (decryption, enc_dec=1) as next key using multiplexers
+  always_comb begin
+    current_key = enc_dec ? message[(chunk)*n*8-1:(chunk-1)*n*8] : out[(chunk)*n*8-1:(chunk-1)*n*8];
+  end
+
   xor_chunk # (
     .n(n)
   ) chunk_after (
     .chunk(message[(chunk+1)*n*8-1:chunk*n*8]),
-    .key(out[(chunk)*n*8-1:(chunk-1)*n*8]),  // Output of previous chunk is key for the next chunk
+    .key(current_key),  // Output of previous chunk is key for the next chunk
     .out(out[(chunk+1)*n*8-1:chunk*n*8])
   );
 
