57b0f270813250e9006516a7545e87ed5048b1f1
AlexanderRitter02 <42546705+AlexanderRitter02@users.noreply.github.com>
2025-06-08 15:45:48 +0200

    Ex05 1f: Synthesis and vcd simulation

---
+++ ex05/Programming_Task_2/post_sim/run.lnx8664.22.09.d/.timestamp.ts
@@ -0,0 +1,4 @@
+1749383162 /home/ritterale/ex_DDaSoES_SS25/ex05/Programming_Task_2/synthesis/syn_pipeline.v
+1689090209 /nfs/wsi/es/eda/Nangate/stdc/si2/instances/latest/verilog/NangateOpenCellLibrary.v
+1749319447 /home/ritterale/ex_DDaSoES_SS25/ex05/Programming_Task_2/tb_cdc.sv
+1749375888 /home/ritterale/ex_DDaSoES_SS25/ex05/Programming_Task_2/tmp_sens.sv
---
+++ ex05/Programming_Task_2/synthesis/gconf.tcl
@@ -0,0 +1,29 @@
+set_db source_verbose false
+read_libs /nfs/wsi/es/eda/Nangate/stdc/si2/instances/latest/liberty/CCS/NangateOpenCellLibrary_typical_ccs.lib
+foreach f {display_package.sv status_module.sv moving_average.sv pipeline.sv tmp_sens.sv} {
+  if { [string match *.vhd ${f}] } {
+    read_hdl -language vhdl ${f} -define USE_CDC=0
+  } elseif { [string match *.v ${f}] } {
+    read_hdl -language v2001 ${f} -define USE_CDC=0
+  } elseif { [string match *.svh ${f}] } {
+    read_hdl -language sv ${f} -define USE_CDC=0
+  } elseif { [string match *.sv ${f}] } {
+    read_hdl -language sv ${f} -define USE_CDC=0
+  }
+}
+set_dont_use "HA*" true
+set_dont_use "FA*" true
+elaborate pipeline
+read_sdc ./synthesis/constraints.sdc
+syn_generic
+syn_map
+syn_opt
+report_power  > ./synthesis/report_power.rpt
+report_gates  > ./synthesis/report_gate.rpt
+report_area   > ./synthesis/report_area.rpt
+foreach {clk} [list clock_slow clock_fast]  {
+  report_timing -group ${clk} > ./synthesis/report_timing_${clk}.rpt
+}
+report_timing -unconstrained > ./synthesis/report_utiming.rpt
+write_hdl pipeline > ./synthesis/syn_pipeline.v
+exit
