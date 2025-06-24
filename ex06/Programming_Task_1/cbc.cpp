b7398f56f930e59335a897f1fb2d39f72310ffa2
Alexander Ritter <42546705+AlexanderRitter02@users.noreply.github.com>
2025-06-24 14:31:07 +0200

    Ex06 3a+b+c: Implement CBC

---
+++ ex06/Programming_Task_1/cbc.cpp
@@ -0,0 +1,37 @@
+
+#include <ap_int.h>
+
+#define m 5;
+#define n 2;
+
+ap_int<n*8> xor_chunk(ap_int<n*8> chunk, ap_int<n*8> key) {
+    return chunk ^ key;
+}
+
+ap_int<n*m*8> crypt_message(bool encrypt_decrypt, ap_int<n*m*8> message, ap_int<n*8> key) {
+
+    ap_int<n*m*8> crypted_message = ; // Only zeros
+    ap_int<n*8> next_key = key;
+
+    for (int i = 0; i < m; i++) {
+
+        if(encrypt_decrypt) {
+            // Decrypt
+            #pragma HLS inline
+            crypted_message[i*n*8 : (i+1)*n*8 - 1] = xor_chunk(
+                crypted_message[i*n*8 : (i+1)*n*8 - 1], next_key)
+
+            next_key = message[i*n*8 : (i+1)*n*8 - 1];
+        } else {
+            // Encrypt
+            #pragma HLS inline
+            next_key = xor_chunk(
+                crypted_message[i*n*8 : (i+1)*n*8 - 1], next_key);
+            crypted_message[i*n*8 : (i+1)*n*8 - 1] = next_key;
+        }
+
+        
+    }
+
+    return crypted_message;
+}
