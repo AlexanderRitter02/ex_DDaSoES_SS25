e15f9a0f27e7ad4b5b793f9cdfb8f6eeabea1077
Alexander Ritter <42546705+AlexanderRitter02@users.noreply.github.com>
2025-06-24 16:14:06 +0200

    Ex06 3: Improve C++ code

--- ex06/Programming_Task_1/cbc.cpp
+++ ex06/Programming_Task_1/cbc.cpp
@@ -1,8 +1,8 @@
 
-#include <ap_int.h>
+#include "ap_int.h"
 
-#define m 5
-#define n 2
+#define m 1
+#define n 1
 
 ap_int<n*8> xor_chunk(ap_int<n*8> chunk, ap_int<n*8> key) {
     return chunk ^ key;
@@ -12,24 +12,20 @@ ap_int<n*m*8> crypt_message(bool encrypt_decrypt, ap_int<n*m*8> message, ap_int<
 
     ap_int<n*m*8> crypted_message = 0; // Only zeros
     ap_int<n*8> next_key = key;
+    ap_int<n*8> crypted_chunk;
+    ap_int<n*8> current_chunk;
 
     for (int i = 0; i < m; i++) {
 
-        if(encrypt_decrypt) {
-            // Decrypt
-            #pragma HLS inline
-            crypted_message((i+1)*n*8 - 1, i*n*8) = xor_chunk(crypted_message((i+1)*n*8 - 1, i*n*8), next_key);
+        current_chunk = message((i+1)*n*8 - 1, i*n*8);
 
-            next_key = message((i+1)*n*8 - 1, i*n*8);
-        } else {
-            // Encrypt
-            #pragma HLS inline
-            next_key = xor_chunk(
-                crypted_message((i+1)*n*8 - 1, i*n*8), next_key);
-            crypted_message((i+1)*n*8 - 1, i*n*8) = next_key;
-        }
+        #pragma HLS inline
+        crypted_chunk = xor_chunk(current_chunk, next_key);
 
-        
+        next_key = (encrypt_decrypt ? current_chunk : crypted_chunk);
+
+        crypted_message((i+1)*n*8 - 1, i*n*8) = crypted_chunk;
+
     }
 
     return crypted_message;
